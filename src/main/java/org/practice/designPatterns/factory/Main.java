dad060a2877713ee6600c6e2b63108e4573646f3
manojsmkg <manojsmk123@gmail.com>
2024-12-23 17:17:27 +0530

    factory pattern example added

--- src/main/java/org/practice/designPatterns/factory/Main.java
+++ src/main/java/org/practice/designPatterns/factory/Main.java
@@ -6,6 +6,7 @@ import org.practice.designPatterns.factory.model.*;
 /*
 Factory Pattern is a creational design pattern that provides an interface for creating objects in superclass
 but allows subclasses to alter the type of objects that will be created.
+eg: JDBC, Swing & JavaFX, Log4j and LogBakc to create factory loggers Serialization frameworks.
  */
 public class Main {
     public static void main(String[] args)
